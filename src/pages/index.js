212a31a585fd130207447dbe1ac41322a394ff6f
Michael Mao <michaelmao@MichaeldeMacBook-Air.local>
2021-10-13 22:20:15 +0800

    test proxy

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,11 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => {
+const HomePage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   )
 }
-export default IndexPage
+export default HomePage
