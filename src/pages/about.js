93e1b9bf603a26d8fdab0a81d8d0c6c41e3c14b3
Michael Mao <michaelmao@MichaeldeMacBook-Air.local>
2021-10-13 20:53:21 +0800

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm a newbie to biulding website. This site is powered by Gatsby and hosted on Netlify.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
