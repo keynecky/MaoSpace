140bfa4723b7c384f2044246696c4cdcbd83bf41
Michael Mao <michaelmao@MichaeldeMacBook-Air.local>
2021-10-14 14:30:55 +0800

    Add a pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,16 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 
 const HomePage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="My profile picture."
+        src="../images/profile-pic.jpeg"
+      />
     </Layout>
   )
 }
